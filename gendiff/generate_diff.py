d553048574392f557ee35013a423faf66347c48a
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-01 23:55:28 +0100

    remove file generate_diff.py

--- gendiff/generate_diff.py
+++
@@ -1,8 +0,0 @@
-from gendiff.diff import diff
-from gendiff.formatter import format_to_strings
-
-
-def generate_diff(path1, path2):
-    gen_diff = diff(path1, path2)
-    format_to_string = format_to_strings(gen_diff)
-    return format_to_string
