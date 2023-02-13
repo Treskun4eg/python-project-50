be21713fd7579efc5c3fd0621ea3babdf7ce81b6
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-13 23:08:04 +0100

    Fix fixture file2_dict()

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,17 +54,51 @@ def path2_yml():
 @pytest.fixture
 def file1_dict():
     return {
-        "host": "hexlet.io",
-        "timeout": 50,
-        "proxy": "123.234.53.22",
-        "follow": False
+        'common': {
+            'setting1': 'Value 1',
+            'setting2': 200,
+            'setting3': True,
+            'setting6': {
+                'key': 'value',
+                'doge': {'wow': ''}
+            }
+        },
+        'group1': {
+            'baz': 'bas',
+            'foo': 'bar',
+            'nest': {'key': 'value'}
+        },
+        'group2': {
+            'abc': 12345,
+            'deep': {'id': 45}
+        }
     }
 
 
 @pytest.fixture
 def file2_dict():
     return {
-        "timeout": 20,
-        "verbose": True,
-        "host": "hexlet.io"
+        'common': {
+            'follow': False,
+            'setting1': 'Value 1',
+            'setting3': None,
+            'setting4': 'blah blah',
+            'setting5': {'key5': 'value5'},
+            'setting6': {
+                'key': 'value',
+                'ops': 'vops',
+                'doge': {'wow': 'so much'}
+            }
+        },
+        'group1': {
+            'foo': 'bar',
+            'baz': 'bars',
+            'nest': 'str'
+        },
+        'group3': {
+            'deep': {
+                'id': {'number': 45}
+            },
+            'fee': 100500
+        }
     }
