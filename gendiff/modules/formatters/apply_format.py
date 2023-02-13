a7e05cdb6ecef89b1375eedcb3587154c170472f
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-13 23:03:14 +0100

    Added new options for displaying the difference 'plain' and 'json'

--- gendiff/modules/formatters/apply_format.py
+++ gendiff/modules/formatters/apply_format.py
@@ -1,6 +1,12 @@
 from gendiff.modules.formatters.stylish import format_to_strings
+from gendiff.modules.formatters.plain import plain_format
+from gendiff.modules.formatters.json import json_format
 
 
-def apply_format(diff_result, format='stylish'):
+def apply_format(diff_result, format):
     if format == 'stylish':
         return format_to_strings(diff_result)
+    if format == 'plain':
+        return plain_format(diff_result)
+    if format == 'json':
+        return json_format(diff_result)
