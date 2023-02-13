b290952869117d7f4aff40c3c2a2bb6c37c32c42
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-13 23:27:53 +0100

    Add tests formatters "json", "plain", "stylish"

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,84 +1,39 @@
-import json
+import ast
 import pytest
-from gendiff.formatter import style_the_value
-from gendiff.formatter import get_string
-from gendiff.formatter import format_to_strings
+from gendiff.modules.formatters import stylish
+from gendiff.modules.formatters import plain
+from gendiff.modules.formatters import json
 
 
 @pytest.fixture
-def open_file3():
-    return json.load(open('tests/fixtures/file3.json'))
+def diff_example():
+    with open('tests/fixtures/diff_result.txt', 'r') as result:
+        diff_result = ast.literal_eval(result.read())
+        return diff_result
 
 
-@pytest.fixture
-def open_file4():
-    return json.load(open('tests/fixtures/file4.json'))
+def test_get_string():
+    assert stylish.get_string({"key": "host", "timeout": 50}, "key", 1) == '    host: host'
 
 
-def test_style_the_value(open_file3):
-    for key, value in open_file3.items():
-        if isinstance(value, dict):
-            assert style_the_value(value, 1) == \
-                   f'{"{"}\n' \
-                   f'    bool: true\n' \
-                   f'    None: null\n' \
-                   f'    string: test string\n' \
-                   f'  {"}"}'
-        elif isinstance(value, bool):
-            assert style_the_value(value, 1) == str(value).lower()
-        elif value is None:
-            assert style_the_value(value, 1) == 'null'
-        else:
-            assert style_the_value(value, 1) == str(value)
+def test_format_to_string(diff_example):
+    with open('tests/fixtures/result_stylish.txt', 'r') as result:
+        assert stylish.format_to_strings(diff_example) == result.read()
 
 
-def test_get_string(open_file4):
-    for key, value in open_file4.items():
-        if value["operation"] == "added":
-            assert get_string(value, "value", 1, "+ ") == \
-                   f'  + {value["key"]}: {value["value"]}'
+def test_plain_format(diff_example):
+    with open('tests/fixtures/result_plain.txt', 'r') as result:
+        assert plain.plain_format(diff_example) == result.read()
 
 
-def test_format_to_strings():
-    diff_result = {
-        'follow':
-            {
-                'key': 'follow',
-                'value': False,
-                'operation': 'deleted'
-            },
-        'host':
-            {
-                'key': 'host',
-                'value': 'hexlet.io',
-                'operation': 'unchanged'
-            },
-        'proxy':
-            {
-                'key': 'proxy',
-                'value': '123.234.53.22',
-                'operation': 'deleted'
-            },
-        'timeout':
-            {
-                'key': 'timeout',
-                'old': 50,
-                'new': 20,
-                'operation': 'changed'
-            },
-        'verbose':
-            {
-                'key': 'verbose',
-                'value': True,
-                'operation': 'added'
-            }
-    }
-    expected = '{\n' \
-               '  - follow: false\n' \
-               '    host: hexlet.io\n' \
-               '  - proxy: 123.234.53.22\n' \
-               '  - timeout: 50\n' \
-               '  + timeout: 20\n' \
-               '  + verbose: true\n' \
-               '}'
-    assert format_to_strings(diff_result) == expected
+def test_json_format(diff_example):
+    with open('tests/fixtures/result_json.txt') as result:
+        assert json.json_format(diff_example) == result.read()
+
+
+def test_style_the_value():
+    assert stylish.style_the_value({'a': 234, 'b': True, 'c': 'value'}, 0) == '{\n' \
+                                                                      '    a: 234\n' \
+                                                                      '    b: true\n' \
+                                                                      '    c: value\n' \
+                                                                      '}'
