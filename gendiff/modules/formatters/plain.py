53a2937bc5ec956cf51c7776dcf49ef2de67596c
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-14 00:06:45 +0100

    Fix syntax errors

--- gendiff/modules/formatters/plain.py
+++ gendiff/modules/formatters/plain.py
@@ -10,16 +10,19 @@ def plain_value(value):
     return f"'{value}'"
 
 
-def plain_format(diff_result: dict):
+def plain_format(diff_result: dict): # noqa C901
 
     def walk(node, path):
         result = ''
         for key, value in node.items():
             current_path = f"{path}{value['key']}"
             if value['operation'] == 'added':
-                result += f"Property '{current_path}' was added {plain_value(value['value'])}\n"
+                result += f"Property '{current_path}'" \
+                          f" was added {plain_value(value['value'])}\n"
             elif value['operation'] == 'changed':
-                result += f"Property '{current_path}' was updated. From {plain_value(value['old'])} to {plain_value(value['new'])}\n"
+                result += f"Property '{current_path}' was updated." \
+                          f" From {plain_value(value['old'])}" \
+                          f" to {plain_value(value['new'])}\n"
             elif value['operation'] == 'deleted':
                 result += f"Property {current_path} was removed\n"
             elif value['operation'] == 'nested':
