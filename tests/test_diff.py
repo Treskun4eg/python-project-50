ba03eafdae0c53063ded222751c25df6cc5e957c
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-13 23:23:22 +0100

    Fix test file diff.py

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,70 +1,23 @@
-import json
+import ast
 import pytest
-from gendiff.diff import open_file
-from gendiff.diff import compare_files
-from gendiff.diff import diff
+from gendiff.modules.diff import compare_files, diff
 
 
-@pytest.fixture
-def open_file1():
-    return json.load(open('tests/fixtures/file1.json'))
+def test_compare_files(file1, file2):
+    added_key, deleted_key, common_key = compare_files(file1, file2)
+    assert added_key == {"d"}
+    assert deleted_key == {"c"}
+    assert common_key == {"a", "b"}
 
 
-@pytest.fixture
-def open_file2():
-    return json.load(open('tests/fixtures/file2.json'))
+parameters = [('file1', 'file2', 'tests/fixtures/diff_result_simple.txt'),
+              ('file1_dict', 'file2_dict', 'tests/fixtures/diff_result.txt')]
 
 
-def test_open_file(open_file1):
-    assert open_file('tests/fixtures/file1.json') == open_file1
-
-
-def test_compare_files(open_file1, open_file2):
-    dict1 = open_file1
-    dict2 = open_file2
-    compare_dictionaries = (
-        dict2.keys() - dict1.keys(),
-        dict1.keys() - dict2.keys(),
-        dict1.keys() & dict2.keys()
-    )
-    assert compare_files(dict1, dict2) == compare_dictionaries
-
-
-def test_generate_diff():
-    path1 = 'tests/fixtures/file1.json'
-    path2 = 'tests/fixtures/file2.json'
-    expected_tree = {
-        'follow':
-            {
-                'key': 'follow',
-                'value': False,
-                'operation': 'deleted'
-            },
-        'host':
-            {
-                'key': 'host',
-                'value': 'hexlet.io',
-                'operation': 'unchanged'
-            },
-        'proxy':
-            {
-                'key': 'proxy',
-                'value': '123.234.53.22',
-                'operation': 'deleted'
-            },
-        'timeout':
-            {
-                'key': 'timeout',
-                'old': 50,
-                'new': 20,
-                'operation': 'changed'
-            },
-        'verbose':
-            {
-                'key': 'verbose',
-                'value': True,
-                'operation': 'added'
-            }
-    }
-
-    assert diff(path1, path2) == expected_tree
+@pytest.mark.parametrize('arg1, arg2, expected', parameters)
+def test_diff(arg1, arg2, expected, request):
+    arg1_value = request.getfixturevalue(arg1)
+    arg2_value = request.getfixturevalue(arg2)
+    with open(expected) as result:
+        diff_resul = ast.literal_eval(result.read())
+        assert diff(arg1_value, arg2_value) == diff_resul
