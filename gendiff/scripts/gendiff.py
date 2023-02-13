a987f7a8c261637a9820221f7e79d2a8ba64e2a8
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-13 23:06:59 +0100

    Added the ability to select the output format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', default='stylish',
-                        choices=['stylish'],
+                        choices=['stylish', 'plain', 'json'],
                         help='set format of output')
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, format=args.format))
