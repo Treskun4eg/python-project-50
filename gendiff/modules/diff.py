9ecd4adf5e961c3e33f74ac1c457208a0df804a4
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-19 21:24:49 +0100

    Dedicated parsing into a separate module

--- gendiff/modules/diff.py
+++ gendiff/modules/diff.py
@@ -1,31 +1,31 @@
-def compare_files(dict1, dict2):
-    added_key = dict2.keys() - dict1.keys()
-    deleted_key = dict1.keys() - dict2.keys()
-    common_key = dict1.keys() & dict2.keys()
-    return added_key, deleted_key, common_key
+def common_and_different(dict1, dict2):
+    added_keys = dict2.keys() - dict1.keys()
+    deleted_keys = dict1.keys() - dict2.keys()
+    common_keys = dict1.keys() & dict2.keys()
+    return added_keys, deleted_keys, common_keys
 
 
 def diff(dict1, dict2):
     keys = sorted(dict1.keys() | dict2.keys())
-    added_key, deleted_key, common_key = compare_files(dict1, dict2)
+    added_keys, deleted_keys, common_keys = common_and_different(dict1, dict2)
     new_dict = {}
     for key in keys:
-        if key in added_key:
+        if key in added_keys:
             description = {'key': key,
                            'value': dict2[key],
                            'operation': 'added'}
             new_dict[key] = description
-        elif key in deleted_key:
+        elif key in deleted_keys:
             description = {'key': key,
                            'value': dict1[key],
                            'operation': 'deleted'}
             new_dict[key] = description
-        elif key in common_key and dict1[key] == dict2[key]:
+        elif key in common_keys and dict1[key] == dict2[key]:
             description = {'key': key,
                            'value': dict1[key],
                            'operation': 'unchanged'}
             new_dict[key] = description
-        elif all([key in common_key,
+        elif all([key in common_keys,
                   dict1[key] != dict2[key],
                   isinstance(dict1[key], dict),
                   isinstance(dict2[key], dict)]
