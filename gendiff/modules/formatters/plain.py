8c285082a2b86e284ae8c359bb85112728aff4c1
Treskun4eg <wagamamanowelli@gmail.com>
2023-02-23 00:28:51 +0100

    Added str.join() method to formatter 'plain'

--- gendiff/modules/formatters/plain.py
+++ gendiff/modules/formatters/plain.py
@@ -13,20 +13,24 @@ def plain_value(value):
 def plain_format(diff_result: dict): # noqa C901
 
     def walk(node, path):
-        result = ''
+        list_result = []
         for key, value in node.items():
             current_path = f"{path}{value['key']}"
             if value['operation'] == 'added':
-                result += f"Property '{current_path}'" \
-                          f" was added with value:" \
-                          f" {plain_value(value['value'])}\n"
+                string = f"Property '{current_path}'" \
+                         f" was added with value:" \
+                         f" {plain_value(value['value'])}"
+                list_result.append(string)
             elif value['operation'] == 'changed':
-                result += f"Property '{current_path}' was updated." \
-                          f" From {plain_value(value['old'])}" \
-                          f" to {plain_value(value['new'])}\n"
+                string = f"Property '{current_path}' was updated." \
+                         f" From {plain_value(value['old'])}" \
+                         f" to {plain_value(value['new'])}"
+                list_result.append(string)
             elif value['operation'] == 'deleted':
-                result += f"Property '{current_path}' was removed\n"
+                string = f"Property '{current_path}' was removed"
+                list_result.append(string)
             elif value['operation'] == 'nested':
-                result += walk(value['value'], current_path + '.') + '\n'
-        return result[:-1]
+                string = walk(value['value'], current_path + '.')
+                list_result.append(string)
+        return '\n'.join(list_result)
     return walk(diff_result, '')
